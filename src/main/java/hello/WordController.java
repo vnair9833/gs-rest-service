7d33cb8d1723e1769745e5e996e0132d33e50116
vnair9833 <44890225+vnair9833@users.noreply.github.com>
2018-11-09 13:38:11 +0530

    Update WordController.java

--- src/main/java/hello/WordController.java
+++ src/main/java/hello/WordController.java
@@ -5,4 +5,59 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WordController {
     // TODO Implement the /words/{word} endpoint
+    
+    
+    	static final int NO_OF_CHARS = 256; 
+
+
+         @GET
+		 @Path("/{words}")
+        public void checkPalindromeAnag(@PathParam("word") String str)
+	{ 
+
+		
+		String reverse = new StringBuffer(str).reverse().toString();
+        String palindrome = "false";
+         boolean pal = false;
+         boolean anag=false;
+         
+		
+	
+
+        if (reverse.equals(str))
+{
+            pal = true;
+}
+         
+if (pal==false)
+{
+		int[] count = new int[NO_OF_CHARS]; 
+
+
+		for (int i = 0; i < str.length(); i++) 
+			count[str.charAt(i)]++; 
+
+		
+		int odd = 0; 
+		for (int i = 0; i < NO_OF_CHARS; i++) { 
+			if ((count[i] & 1) != 0) 
+				odd++; 
+
+			if (odd > 1) 
+				anag=false; 
+				else
+				anag=true;
+		} 
+	
+		
+		JSONObject obj = new JSONObject();
+
+      obj.put("word", str);
+      obj.put("palindrome",pal);
+      obj.put("anagramOfPalindrome",anag);
+      System.out.print(obj);
+}	
+
+    
+}
 }
